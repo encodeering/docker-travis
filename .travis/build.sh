5c435620c4b99cebc5a70fe1083430d52dba051c
Michael Clausen <encodeering@gmail.com>
2016-01-13 20:02:44 +0100

    container should be executed once
    * and print the travis version; can be seen as verification and debug/audit step

--- .travis/build.sh
+++ .travis/build.sh
@@ -9,3 +9,5 @@ docker pull   "$REPOSITORY/ruby-$ARCH:2.3"
 docker tag -f "$REPOSITORY/ruby-$ARCH:2.3" "ruby:2.3"
 
 docker build -t "$TAG:$TAGSPECIFIER" --build-arg VERSION="$VERSION" "$PROJECT"
+
+docker run --rm "$TAG:$TAGSPECIFIER" version
