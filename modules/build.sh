0722b4aa545c2d8bfa19a17e86a56e3593629f5a
Michael Clausen <encodeering@gmail.com>
2018-01-02 14:22:16 +0100

    switches to ci package
    * taking the latest release of 0.1.4
    
    NOTE
    
    * all docker images should be located under module

---
+++ modules/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+import com.encodeering.docker.config
+import com.encodeering.docker.docker
+
+docker-pull "$REPOSITORY/ruby-$ARCH:2.3" "ruby:2.3"
+
+docker build -t "$DOCKER_IMAGE" --build-arg VERSION="$VERSION" foreman
+
+docker run --rm "$DOCKER_IMAGE" travis version
