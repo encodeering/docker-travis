32b9e248e7bd9c6958264669b248dfea24731470
Michael Clausen <encodeering@gmail.com>
2018-01-07 13:11:04 +0100

    fixes base image definition
    * ruby image can't be used without base image discriminator anymore

--- modules/build.sh
+++ modules/build.sh
@@ -5,7 +5,7 @@ set -e
 import com.encodeering.docker.config
 import com.encodeering.docker.docker
 
-docker-pull "$REPOSITORY/ruby-$ARCH:2.3" "ruby:2.3"
+docker-pull "$REPOSITORY/ruby-$ARCH:2.3-debian" "ruby:2.3"
 
 docker build -t "$DOCKER_IMAGE" --build-arg VERSION="$VERSION" foreman
 
