19b36c32360604eb4505f33899b4459b238f09d8
Michael Clausen <encodeering@gmail.com>
2019-03-17 12:11:24 +0100

    verifies if the binary responds with a correct image version
    * image tag should match the version of the binary

--- modules/build.sh
+++ modules/build.sh
@@ -9,4 +9,4 @@ docker-pull "$REPOSITORY/ruby-$ARCH:2.3-debian" "ruby:2.3"
 
 docker-build --build-arg VERSION="$PIN" foreman
 
-docker-verify travis version
+docker-verify travis version | dup | matches "^${PIN}"
